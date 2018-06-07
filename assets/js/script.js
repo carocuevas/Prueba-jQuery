af2b2f0180321d62fdf85d110b340c93d3b271c2
carocuevas <catecuevas@gmail.com>
2018-06-06 21:48:13 -0400

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		var data = $('#sendtweet').val();
 		var likes = 1 + Math.floor(Math.random() * 10);
 
-		var element = '<li><div class="Tweet_block"><span>' + data + '</span><a class="add__tweet__like" href="#" >&nbsp;&nbsp;<i class="fas fa-heart">&nbsp;<p class="likeable">' + likes + '</p>&nbsp;</i></a>&nbsp;<a class="add__tweet__remove" href="#"><i class="fas fa-trash"></i></a></div></li>';
+		var element = '<li><div class="Tweet_block"><span>' + data + '</span><a class="likeable" href="#" >&nbsp;&nbsp;<i class="fas fa-heart add__tweet__like">&nbsp;<p class="contador">' + likes + '</p>&nbsp;</i></a>&nbsp;<a class="add__tweet__remove" href="#"><i class="fas fa-trash"></i></a></div></li>';
 		
 		$('.add__tweet').append(element);
 		$('#sendtweet').val('');
@@ -20,7 +20,10 @@ $(document).ready(function(){
 	$('ul').on('click', '.add__tweet__like', function(event){
 		event.preventDefault();
 		event.stopPropagation();
-		$(this).toggleClass('.add__tweet__like--clicked');
+		$(this).toggleClass('add__tweet__like--clicked');
+		var hearts = parseInt($(this).children('.contador').text());
+		console.log(hearts++)
+		$(this).children('.contador').text(hearts)
 	});
 
 // Eliminar tweet
@@ -29,19 +32,6 @@ $(document).ready(function(){
 		event.preventDefault();
 		event.stopPropagation();
 		$(this).parent().remove();
-	});
-
-	$('ul').on('click', '.add__tweet__like', function(event){
-		event.preventDefault();
-		event.stopPropagation();
-		var heart = parseInt($(likes).val()) + 1;
-		$('likes').show().val(heart);
-
-		console.log(heart)
-
-
-
-
 	});
 
 });
